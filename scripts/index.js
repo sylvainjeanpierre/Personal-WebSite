8d6196648f7e767e374803789bd48ae7ac0fe11c
Sylvain JEANPIERRE <112515717+sylvainjeanpierre@users.noreply.github.com>
2024-03-20 19:01:52 +0100

    internal section and network links
    - internal section links with scroll effect
    - contact and projects hover effect
    - github and linkendin links in home and footer

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+// management of page section links
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".section_target_btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const targetId = button.getAttribute("data-target");
+            const section = document.getElementById(targetId);
+
+            if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
+
+
+
+// animation of home page button arrows
+function handleButtonHover(button, arrow) {
+    button.addEventListener('mouseover', () => {
+        arrow.style.transition = '200ms';
+        arrow.style.position = 'relative';
+        arrow.style.top = '50px';
+        arrow.style.color = 'rgb(255, 100, 100)'
+    });
+
+    button.addEventListener('mouseleave', () => {
+        arrow.style.transition = '200ms';
+        arrow.style.position = 'relative';
+        arrow.style.top = '0px';
+        arrow.style.color = 'rgb(0, 0, 0)'
+    });
+}
+
+const buttonContact = document.getElementById('home__button-contact');
+const arrowContact = document.querySelector('#home__button-contact span');
+handleButtonHover(buttonContact, arrowContact);
+
+const projectsBtn = document.getElementById('home__button-projects');
+const projectsArrow = document.querySelector('#home__button-projects span');
+handleButtonHover(projectsBtn, projectsArrow);
+
+
+
+// redirection of network links
+const githubBtns = document.querySelectorAll(".github-link");
+const linkedinBtns = document.querySelectorAll(".linkedin-link");
+
+githubBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.open("https://github.com/sylvainjeanpierre", "_blank");
+    });
+});
+
+linkedinBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.open("https://www.linkedin.com/in/sylvain-jeanpierre-a25a14128/", "_blank");
+    });
+});
